d66843c2423e36fd7c0d155ee67abbdea4ae7d52
RoTour <robin.tourne@ynov.com>
2020-11-21 21:09:37 +0100

    + first steps with firestore

--- Projects/WIP/SuperPlanning/app/src/main/java/com/novyapp/superplanning/data/Models.kt
+++ Projects/WIP/SuperPlanning/app/src/main/java/com/novyapp/superplanning/data/Models.kt
@@ -1,5 +1,8 @@
 package com.novyapp.superplanning.data
 
+import com.google.firebase.Timestamp
+import com.google.firebase.firestore.DocumentSnapshot
+
 data class Course(
     var subject: String = "",
     var teacher: String = "",
@@ -10,3 +13,22 @@ data class Course(
 }
 
 data class CourseListViews(val viewType: Int, val course: Course)
+
+
+data class CourseV2 (
+    val date: Timestamp? = null,
+    val professor: String? = null,
+    val subject: String? = null,
+    val classroom: String? = null
+) {
+
+}
+
+fun DocumentSnapshot.toCourseV2(): CourseV2{
+    return CourseV2(
+        this.data?.get("datetime") as Timestamp,
+        this.data?.get("professor") as String,
+        this.data?.get("subject") as String,
+        this.data?.get("classroom") as String
+    )
+}
--- Projects/WIP/SuperPlanning/app/src/main/java/com/novyapp/superplanning/ui/main/MainPage.kt
+++ Projects/WIP/SuperPlanning/app/src/main/java/com/novyapp/superplanning/ui/main/MainPage.kt
@@ -11,6 +11,12 @@ import androidx.fragment.app.viewModels
 import com.firebase.ui.auth.AuthUI
 import com.firebase.ui.auth.IdpResponse
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.firestore.ktx.toObject
+import com.google.firebase.ktx.Firebase
+import com.novyapp.superplanning.data.CourseV2
+import com.novyapp.superplanning.data.toCourseV2
 import com.novyapp.superplanning.databinding.MainPageFragmentBinding
 import timber.log.Timber
 
@@ -20,47 +26,90 @@ private const val REQUEST_CODE = 12
 // TODO("display the current week and not this 1st week available")
 class MainPage : Fragment() {
 
+    private lateinit var db: FirebaseFirestore
     private lateinit var binding: MainPageFragmentBinding
     private val viewModel by viewModels<MainPageViewModel> {
         MainPageViewModelFactory()
     }
 
     override fun onCreateView(
-            inflater: LayoutInflater, container: ViewGroup?,
-            savedInstanceState: Bundle?
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
     ): View? {
         binding = MainPageFragmentBinding.inflate(inflater, container, false)
-        if (FirebaseAuth.getInstance().currentUser == null) launchSignInFlow()
-
-
-        val adapter = ViewPagerAdapter()
-        binding.viewPager.adapter = adapter
-
-
-
-        viewModel.courses.observe(viewLifecycleOwner) {
-            if (it.isNullOrEmpty()) Timber.i("Course list is Null or Empty")
-            else {
-                it.forEach { item -> Timber.i(item.toString()) }
-                adapter.submitList(it)
-                Timber.i("List submitted to adapter")
-            }
+        db = Firebase.firestore
+        if (FirebaseAuth.getInstance().currentUser == null) {
+            Timber.i("Need to connect user")
+            launchSignInFlow()
+        } else {
+            Timber.i("User is already connected")
+            testFireStore()
         }
+
+
+//        val adapter = ViewPagerAdapter()
+//        binding.viewPager.adapter = adapter
+//
+//
+//
+//        viewModel.courses.observe(viewLifecycleOwner) {
+//            if (it.isNullOrEmpty()) Timber.i("Course list is Null or Empty")
+//            else {
+//                it.forEach { item -> Timber.i(item.toString()) }
+//                adapter.submitList(it)
+//                Timber.i("List submitted to adapter")
+//            }
+//        }
 
         return binding.root
     }
 
+    private fun testFireStore() {
+//        val user = hashMapOf(
+//            "first" to "Ada",
+//            "last" to "Lovelace",
+//            "born" to 1815
+//        )
+//
+//        // Add a new document with a generated ID
+//        db.collection("users")
+//            .add(user)
+//            .addOnSuccessListener { documentReference ->
+//                Timber.i("DocumentSnapshot added with ID: ${documentReference.id}")
+//            }
+//            .addOnFailureListener { e ->
+//                Timber.i(e)
+//            }
+
+        val docRef = db
+//            .collection("/Courses/B1-INFO-2020-2021/Weeks")
+//            .document("40")
+//            .collection("Weeks")
+            .collection("/Courses/B1-INFO-2020-2021/Weeks/40/planning")
+        docRef.get()
+            .addOnSuccessListener { result ->
+                val usableList = mutableListOf<CourseV2>()
+                result.documents.forEach {
+                    usableList.add(it.toCourseV2())
+                }
+                Timber.i("Usable list of data: \n $usableList")
+            }
+            .addOnFailureListener { e ->
+                Timber.i(e)
+            }
+    }
+
     private fun launchSignInFlow() {
         val providers = arrayListOf(
-                AuthUI.IdpConfig.EmailBuilder().build(),
-                AuthUI.IdpConfig.GoogleBuilder().build()
+            AuthUI.IdpConfig.EmailBuilder().build(),
+            AuthUI.IdpConfig.GoogleBuilder().build()
         )
         startActivityForResult(
-                AuthUI.getInstance()
-                        .createSignInIntentBuilder()
-                        .setAvailableProviders(providers)
-                        .build(),
-                REQUEST_CODE
+            AuthUI.getInstance()
+                .createSignInIntentBuilder()
+                .setAvailableProviders(providers)
+                .build(),
+            REQUEST_CODE
         )
     }
 
@@ -71,6 +120,7 @@ class MainPage : Fragment() {
             if (resultCode == Activity.RESULT_OK) {
                 // User successfully signed in
                 Timber.i("Successfully signed in user ${FirebaseAuth.getInstance().currentUser?.displayName}!")
+                testFireStore()
             } else {
                 // Sign in failed. If response is null the user canceled the
                 // sign-in flow using the back button. Otherwise check
