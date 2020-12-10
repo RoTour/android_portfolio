5378fcbd06290abdf3875cf467b1890185f7702a
RoTour <robin.tourne@ynov.com>
2020-12-10 15:25:37 +0100

    Cleaned up Utils.kt Todo: update list when a course is added

--- Projects/WIP/SuperPlanning/app/src/main/java/com/novyapp/superplanning/Utils.kt
+++ Projects/WIP/SuperPlanning/app/src/main/java/com/novyapp/superplanning/Utils.kt
@@ -9,39 +9,6 @@ import java.util.*
 import kotlin.collections.HashMap
 import kotlin.collections.LinkedHashMap
 
-fun String.fromISOtoNice(): String {
-    return "${this.substring(0, 4)}/${this.substring(5, 7)}/${this.substring(8, 10)}"
-}
-
-fun Calendar.toISOString(): String {
-    val inputString = "${this.get(Calendar.YEAR)}/${this.get(Calendar.MONTH)+1}/${this.get(Calendar.DAY_OF_MONTH)} ${this.get(Calendar.HOUR_OF_DAY)}:${this.get(Calendar.MINUTE)}"
-    val date: Date = SimpleDateFormat("yyyy/MM/dd HH:mm").parse(inputString) ?: Date()
-
-    val sdf = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSXXX")
-    return sdf.format(date)
-}
-
-fun String.fromISOtoCalendar(): Calendar {
-    val cal = Calendar.getInstance()
-    cal.set(
-            this.substring(0,4).toInt(), // Year
-            this.substring(5,7).toInt() -1, // Month
-            this.substring(8,10).toInt(), // Day
-            this.substring(11,13).toInt(), // Hour
-            this.substring(14,16).toInt() // Min
-    )
-    return cal
-}
-
-fun Int.nextWeekNumber(): Int{
-    if(this <= 0 || this > 52) throw IllegalArgumentException("WeekNumber should be in 0 < WeekNumber <= 52")
-    return if(this!=52)  this+1 else 1
-}
-
-fun Int.previousWeekNumber(): Int{
-    if(this <= 0 || this > 52) throw IllegalArgumentException("WeekNumber should be in 0 < WeekNumber <= 52")
-    return if(this!=1)  this-1 else 52
-}
 
 fun todayWeekNumber(): Int {
     return Calendar.getInstance().get(Calendar.WEEK_OF_YEAR)
@@ -68,12 +35,3 @@ fun LinkedHashMap<String, MutableList<CourseV2>>.toSortedByWeeksMap(): LinkedHas
     Timber.i("newWeek: sortedmap : $sortedMap")
     return sortedMap
 }
-
-
-//fun numberOfWeeksInYear(year: Int = Calendar.getInstance().get(Calendar.YEAR)): Int {
-//    val cal = Calendar.getInstance()
-//    cal.set(year, 11,31)
-//    Timber.i("newWeek: ${DateFormat.getDateInstance(DateFormat.LONG).format(Date(cal.timeInMillis))}")
-//    Timber.i("newWeek: ${cal.get(Calendar.WEEK_OF_YEAR)}")
-//    return 52
-//}
